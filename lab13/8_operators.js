0b73e54834d9ed6ca73b71583cd28cf155b638ad
legalitySH <rpsher@bk.ru>
2024-06-03 22:43:11 +0300

    lab_13

---
+++ lab13/8_operators.js
@@ -0,0 +1,6 @@
+var key = 'name';
+key = 'age';
+var u1 = 'name';
+var u2 = { 'name': 'aboba', 'email': 'aboba@mail.ru' };
+// u1 = '_id';
+// u2 = 'name';
---
+++ lab13/8_operators.ts
@@ -0,0 +1,25 @@
+interface Person {
+  name: string
+  age: number
+}
+
+type PersonKeys = keyof Person // 'name' | 'age'
+
+let key: PersonKeys = 'name'
+key = 'age'
+
+type User = {
+  _id: number
+  name: string
+  email: string
+  createdAt: Date
+}
+
+type UserKeysNoMeta1 = Exclude<keyof User, '_id' | 'createdAt'> // 'name' | 'email'
+type UserKeysNoMeta2 = Pick<User, 'name' | 'email'> // 'name' | 'email'
+
+
+let u1: UserKeysNoMeta1 = 'name';
+let u2: UserKeysNoMeta2 = {'name' : 'aboba' , 'email' : 'aboba@mail.ru'};
+// u1 = '_id';
+// u2 = 'name';
